db138007dc8dda42fd4e853c3e16e7fa6b3ba63c
Jacob Miller <jacob@vsi-labs.com>
2020-11-05 17:26:19 -0500

    switched to main

---
+++ code/submission.py
@@ -0,0 +1,342 @@
+# ##################################################################### #
+# 16720B: Computer Vision Homework 5
+# Carnegie Mellon University
+# Oct. 26, 2020
+# ##################################################################### #
+
+
+# Insert your package here
+from skimage.color import rgb2xyz
+from scipy.sparse import kron as spkron
+from scipy.sparse import eye as speye
+from scipy.sparse.linalg import lsqr as splsqr
+import pdb
+from utils import integrateFrankot
+
+
+'''
+Q3.2.1: Eight Point Algorithm
+    Input:  pts1, Nx2 Matrix
+            pts2, Nx2 Matrix
+            M, a scalar parameter computed as max (imwidth, imheight)
+    Output: F, the fundamental matrix
+'''
+def eightpoint(pts1, pts2, M):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.2.2: Seven Point Algorithm
+    Input:  pts1, Nx2 Matrix
+            pts2, Nx2 Matrix
+            M, a scalar parameter computed as max (imwidth, imheight)
+    Output: Farray, a list of estimated fundamental matrix.
+'''
+def sevenpoint(pts1, pts2, M):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.3.1: Compute the essential matrix E.
+    Input:  F, fundamental matrix
+            K1, internal camera calibration matrix of camera 1
+            K2, internal camera calibration matrix of camera 2
+    Output: E, the essential matrix
+'''
+def essentialMatrix(F, K1, K2):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points.
+    Input:  C1, the 3x4 camera matrix
+            pts1, the Nx2 matrix with the 2D image coordinates per row
+            C2, the 3x4 camera matrix
+            pts2, the Nx2 matrix with the 2D image coordinates per row
+    Output: P, the Nx3 matrix with the corresponding 3D points per row
+            err, the reprojection error.
+'''
+def triangulate(C1, pts1, C2, pts2):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.4.1: 3D visualization of the temple images.
+    Input:  im1, the first image
+            im2, the second image
+            F, the fundamental matrix
+            x1, x-coordinates of a pixel on im1
+            y1, y-coordinates of a pixel on im1
+    Output: x2, x-coordinates of the pixel on im2
+            y2, y-coordinates of the pixel on im2
+
+'''
+def epipolarCorrespondence(im1, im2, F, x1, y1):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.5.1: RANSAC method.
+    Input:  pts1, Nx2 Matrix
+            pts2, Nx2 Matrix
+            M, a scaler parameter
+    Output: F, the fundamental matrix
+            inliers, Nx1 bool vector set to true for inliers
+'''
+def ransacF(pts1, pts2, M):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.5.2: Rodrigues formula.
+    Input:  r, a 3x1 vector
+    Output: R, a rotation matrix
+'''
+def rodrigues(r):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.5.2: Inverse Rodrigues formula.
+    Input:  R, a rotation matrix
+    Output: r, a 3x1 vector
+'''
+def invRodrigues(R):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.5.3: Rodrigues residual.
+    Input:  K1, the intrinsics of camera 1
+            M1, the extrinsics of camera 1
+            p1, the 2D coordinates of points in image 1
+            K2, the intrinsics of camera 2
+            p2, the 2D coordinates of points in image 2
+            x, the flattened concatenationg of P, r2, and t2.
+    Output: residuals, 4N x 1 vector, the difference between original 
+            and estimated projections
+'''
+def rodriguesResidual(K1, M1, p1, K2, p2, x):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.5.3 Bundle adjustment.
+    Input:  K1, the intrinsics of camera 1
+            M1, the extrinsics of camera 1
+            p1, the 2D coordinates of points in image 1
+            K2,  the intrinsics of camera 2
+            M2_init, the initial extrinsics of camera 1
+            p2, the 2D coordinates of points in image 2
+            P_init, the initial 3D coordinates of points
+    Output: M2, the optimized extrinsics of camera 1
+            P2, the optimized 3D coordinates of points
+'''
+def bundleAdjustment(K1, M1, p1, K2, M2_init, p2, P_init):
+    # Replace pass by your implementation
+    pass
+
+
+def renderNDotLSphere(center, rad, light, pxSize, res):
+
+    """
+    Q4.1
+
+    Render a sphere with a given center and radius. The camera is 
+    orthographic and looks towards the sphere in the negative z
+    direction. The camera's sensor axes are centerd on and aligned
+    with the x- and y-axes.
+
+    Parameters
+    ----------
+    center : numpy.ndarray
+        The center of the hemispherical bowl in an array of size (3,)
+
+    rad : float
+        The radius of the bowl
+
+    light : numpy.ndarray
+        The direction of incoming light
+
+    pxSize : float
+        Pixel size
+
+    res : numpy.ndarray
+        The resolution of the camera frame
+
+    Returns
+    -------
+    image : numpy.ndarray
+        The rendered image of the hemispherical bowl
+    """
+    # Replace pass by your implementation
+    pass
+
+
+def loadData(path = "../data/"):
+
+    """
+    Q4.2.1
+
+    Load data from the path given. The images are stored as input_n.tif
+    for n = {1...7}. The source lighting directions are stored in
+    sources.mat.
+
+    Paramters
+    ---------
+    path: str
+        Path of the data directory
+
+    Returns
+    -------
+    I : numpy.ndarray
+        The 7 x P matrix of vectorized images
+
+    L : numpy.ndarray
+        The 3 x 7 matrix of lighting directions
+
+    s: tuple
+        Image shape
+
+    """
+    # Replace pass by your implementation
+    pass
+
+
+def estimatePseudonormalsCalibrated(I, L):
+
+    """
+    Q4.2.2
+
+    In calibrated photometric stereo, estimate pseudonormals from the
+    light direction and image matrices
+
+    Parameters
+    ----------
+    I : numpy.ndarray
+        The 7 x P array of vectorized images
+
+    L : numpy.ndarray
+        The 3 x 7 array of lighting directions
+
+    Returns
+    -------
+    B : numpy.ndarray
+        The 3 x P matrix of pesudonormals
+    """
+    # Replace pass by your implementation
+    pass
+
+
+def estimateAlbedosNormals(B):
+
+    '''
+    Q4.2.3
+
+    From the estimated pseudonormals, estimate the albedos and normals
+
+    Parameters
+    ----------
+    B : numpy.ndarray
+        The 3 x P matrix of estimated pseudonormals
+
+    Returns
+    -------
+    albedos : numpy.ndarray
+        The vector of albedos
+
+    normals : numpy.ndarray
+        The 3 x P matrix of normals
+    '''
+    # Replace pass by your implementation
+    pass
+
+
+def displayAlbedosNormals(albedos, normals, s):
+
+    """
+    Q4.2.4
+
+    From the estimated pseudonormals, display the albedo and normal maps
+
+    Please make sure to use the `coolwarm` colormap for the albedo image
+    and the `rainbow` colormap for the normals.
+
+    Parameters
+    ----------
+    albedos : numpy.ndarray
+        The vector of albedos
+
+    normals : numpy.ndarray
+        The 3 x P matrix of normals
+
+    s : tuple
+        Image shape
+
+    Returns
+    -------
+    albedoIm : numpy.ndarray
+        Albedo image of shape s
+
+    normalIm : numpy.ndarray
+        Normals reshaped as an s x 3 image
+
+    """
+    # Replace pass by your implementation
+    pass
+
+
+def estimateShape(normals, s):
+
+    """
+    Q4.3.1
+
+    Integrate the estimated normals to get an estimate of the depth map
+    of the surface.
+
+    Parameters
+    ----------
+    normals : numpy.ndarray
+        The 3 x P matrix of normals
+
+    s : tuple
+        Image shape
+
+    Returns
+    ----------
+    surface: numpy.ndarray
+        The image, of size s, of estimated depths at each point
+
+    """
+    # Replace pass by your implementation
+    pass
+
+
+def plotSurface(surface):
+
+    """
+    Q4.3.1
+
+    Plot the depth map as a surface
+
+    Parameters
+    ----------
+    surface : numpy.ndarray
+        The depth map to be plotted
+
+    Returns
+    -------
+        None
+
+    """
+    # Replace pass by your implementation
+    pass
---
+++ code/visualize.py
@@ -0,0 +1,11 @@
+# ##################################################################### #
+# 16720B: Computer Vision Homework 5
+# Carnegie Mellon University
+# Oct. 26, 2020
+# ##################################################################### #
+
+'''
+Q3.7:
+    1. Integrating everything together.
+    2. Loads necessary files from ../data/ and visualizes 3D reconstruction using scatter
+'''
